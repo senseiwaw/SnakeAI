f9a7fe8c88566ac2301064e65b64f3e1544c4f61
senseiwaw <hgbekrar@gmail.com>
2025-12-13 22:23:54 +0100

    adding a sprite

--- src/games/snake/main.py
+++ src/games/snake/main.py
@@ -1,7 +1,9 @@
 import arcade
 
+
+
 WINDOW_WIDTH  = 900	
-WINDOW_HEIGHT = 900
+WINDOW_HEIGHT = 600
 WINDOW_TITLE  = "Snake"
 
 class GameView(arcade.Window):
@@ -12,9 +14,14 @@ class GameView(arcade.Window):
     def __init__(self):
 
         # Call the parent class to set up the window
-        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
-
-        self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE,resizable =True)
+        self.background_color = arcade.color.CHARTREUSE
+        self.player_texture = arcade.load_texture(":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png")
+        self.player_sprite = arcade.Sprite(self.player_texture)
+        self.player_sprite.center_x = 300
+        self.player_sprite.center_y = 200
+        self.player_list = arcade.SpriteList()
+        self.player_list.append(self.player_sprite)
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
@@ -30,7 +37,7 @@ class GameView(arcade.Window):
         self.clear()
 
         # Code to draw other things will go here
-
+        self.player_list.draw()
 
 def main():
     """Main function"""
